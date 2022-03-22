470fb67004d33b477418a015f5233bcbc13241ab
Sean Campbell <sean.campbell13@gmail.com>
2022-03-22 07:42:02 -0400

    fix: profile scroll safari?

--- src/components/StaffProfiles.tsx
+++ src/components/StaffProfiles.tsx
@@ -152,6 +152,7 @@ const ProfileCard: React.FC<{ edge: StaffProfilesYamlEdge }> = (props) => {
         sx={{
           boxShadow: 'rgba(245, 245, 245, 0.35) 0px 5px 15px',
           backfaceVisibility: 'hidden',
+          '-webkit-backface-visibility': 'hidden',
         }}
         role="group"
       >
