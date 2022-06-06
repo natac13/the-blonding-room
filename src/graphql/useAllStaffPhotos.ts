d20c5a2c05ef6594497f04b37cc33075c4cd173f
Sean Campbell <sean.campbell13@gmail.com>
2022-06-05 20:31:30 -0400

    feat: add staff bio photos

---
+++ src/graphql/useAllStaffPhotos.ts
@@ -0,0 +1,28 @@
+import { graphql, useStaticQuery } from 'gatsby'
+import { AllStaffPhotosQuery } from '../types/generated-gatsby'
+
+export const useAllStaffPhotos = (): AllStaffPhotosQuery => {
+  const data = useStaticQuery<AllStaffPhotosQuery>(
+    graphql`
+      query AllStaffPhotos {
+        photos: allFile(filter: { relativeDirectory: { eq: "staff" } }) {
+          edges {
+            node {
+              name
+              id
+              publicURL
+              childImageSharp {
+                thumb: gatsbyImageData(layout: FIXED, width: 300, height: 350)
+                full: gatsbyImageData(layout: FULL_WIDTH)
+                # bio: gatsbyImageData(layout: CONSTRAINED)
+                bio: gatsbyImageData(layout: FIXED, width: 370, height: 430)
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return data
+}
